f7ba7f0b1aad9fecd51ac860d1f2a74274c877f0
Kuba Andrysek <jakub.andrysek03@gmail.com>
2020-09-13 11:16:22 +0200

    Add checker for non-existent sensors

--- Pletacka-server/main.py
+++ Pletacka-server/main.py
@@ -9,19 +9,24 @@ import threading
 class MyUDPHandler(socketserver.BaseRequestHandler):
 
     def handle(self):
+        # separate number from message
         sensor_number = str(self.request[0].strip())
         sensor_number = int(re.search(r'\d+', sensor_number)[0])
+
         from_ip = self.client_address[0]
-        # socket = self.request[1]
-        # print("{} wrote:".format(from_ip))
-        # print(sensor_number)
+
+        # check that sensor is in on sensors
         if sensor_number in onSensors.keys():
-            add_on_sensor(sensor_number)
+            reset_cutdown(sensor_number)
         else:
-            # not in keys
-            print("Add - {}".format(sensor_number))
-            add_on_sensor(sensor_number)
-        # print(onSensors)
+            # check that sensor is in database
+            if sensor_number in all_sensors_status.keys():
+                # add sensor in on sensors
+                print("Add - {}".format(sensor_number))
+                reset_cutdown(sensor_number)
+            else:
+                # not existting sensor
+                print("ERROR - adding unexisting sensor -> {}".format(sensor_number))
 
 
 def get_ip():
@@ -41,39 +46,37 @@ def get_on_sensors():
 
         status = db.fetchall()
         if status:
-            all_status[sensor_number] = status[0][0]
+            all_sensors_status[sensor_number] = status[0][0]
             if status[0][0] != "OFF":
                 onSensors[sensor_number] = 10
         else:
-            all_status[sensor_number] = -1
+            all_sensors_status[sensor_number] = -1
 
     return onSensors
 
 
-def add_on_sensor(number):
+def reset_cutdown(number):
     onSensors[number] = 10
-
 
 
 def cutdown_old():
     for sensor in onSensors:
-        if(onSensors[sensor]>2):
-            onSensors[sensor] = onSensors[sensor]-1
+        if onSensors[sensor] > 2:
+            onSensors[sensor] = onSensors[sensor] - 1
         elif onSensors[sensor] == -1:
-            x=""
+            x = ""
         else:
             onSensors[sensor] = -1
             print("Remove - {}".format(sensor))
-
 
     print(onSensors)
     cutdowner = threading.Timer(3, cutdown_old).start()
 
 
-
 if __name__ == "__main__":
     ip = get_ip()
     print("Server IP:" + ip)
+    print("Read from DB: ", end='')
 
     pdb = mysql.connector.connect(
         host="localhost",
@@ -82,20 +85,18 @@ if __name__ == "__main__":
         database="pletacka-ex"
     )
 
-
-
-    all_status = {}
+    all_sensors_status = {}
 
     onSensors = get_on_sensors()
 
     cutdown_old()
 
-
-
-
-    # cutdown_old()
-
-
     HOST, PORT = ip, 2727
     with socketserver.UDPServer((HOST, PORT), MyUDPHandler) as server:
         server.serve_forever()
+
+# //To do
+# - kontrola existence senzoru
+# - odesilani do databaze
+# - autostartup malina
+# - prejmenovat databaze
